a30a8bbd81874d53e30b73c36016dc6e652749be
Stasies <tia.nntr.wth@gmail.com>
2024-12-04 14:33:47 +0300

    test socket.io connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,51 @@
-const Page = () => {
-  return <div>page</div>;
-};
+"use client";
 
-export default Page;
+import { useEffect, useState } from "react";
+import { socket } from "../socket";
+
+export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+      socket.on("response", (data) => {
+        console.log("Server says:", data);
+      });
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    // socket.on("message", (val) => console.log(val));
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+  const sendMessage = (message: string) => {
+    console.log("clicked");
+    socket.emit("message", { message });
+  };
+
+  return (
+    <div>
+      <p>Status: {isConnected ? "connected" : "disconnected"}</p>
+      <p>Transport: {transport}</p>
+      <button onClick={() => sendMessage("potato")}>button</button>
+    </div>
+  );
+}
