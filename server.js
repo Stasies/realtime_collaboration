a92f9b2c12b2cddd91d7e7ac0bf6451566fb8b39
Stasies <tia.nntr.wth@gmail.com>
2024-12-04 14:33:14 +0300

    add socket handlers

---
+++ server.js
@@ -0,0 +1,33 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 1234;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log(socket);
+    socket.on("message", (val) => {
+      console.log("Received message:", val);
+      socket.emit("response", { message: "received" });
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
